3d1f8bd24458732cad1ff807246a1c4579513937
Hardi Maulana <331568215+DynessStation@users.noreply.github.com>
2026-09-22 16:02:18 +0700

    public: add matex-footer

--- frontend/public/src/app/layout/matex-layout/matex-layout.ts
+++ frontend/public/src/app/layout/matex-layout/matex-layout.ts
@@ -1,38 +1,22 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Store } from '@ngxs/store';
-import { MatexHeader } from './matex-header/matex-header';
-import { Footer } from '../../shared/components/footer/footer';
-import { BackToTop } from '../../shared/components/widgets/back-to-top/back-to-top';
-
-import { GetSettingOption } from '../../shared/store/action/setting.action';
-import { ThemeOptions } from '../../shared/store/action/theme-option.action';
-import { GetThemes } from '../../shared/store/action/theme.action';
-
-import { ThemeOptionState } from '../../shared/store/state/theme-option.state';
-
 import { PublicPageContextService } from '../../shared/services/public-page-context.service';
+import { BackToTop } from '../../shared/components/widgets/back-to-top/back-to-top';
+import { MatexFooter } from './matex-footer/matex-footer';
+import { MatexHeader } from './matex-header/matex-header';
 
 @Component({
   selector: 'app-matex-layout',
-  imports: [AsyncPipe, RouterOutlet, MatexHeader, Footer, BackToTop],
+  imports: [RouterOutlet, MatexHeader, MatexFooter, BackToTop],
   templateUrl: './matex-layout.html',
   styleUrl: './matex-layout.scss',
 })
 export class MatexLayout implements OnDestroy {
-  private store = inject(Store);
   private publicPageContext = inject(PublicPageContextService);
-
-  themeOption$ = this.store.select(ThemeOptionState.themeOptions);
 
   constructor() {
     this.publicPageContext.activate();
-
-    this.store.dispatch(new GetThemes());
-    this.store.dispatch(new GetSettingOption());
-    this.store.dispatch(new ThemeOptions());
   }
 
   ngOnDestroy(): void {
